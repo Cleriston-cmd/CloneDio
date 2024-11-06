87e729846cf58e070d1592fc704d03ea9b8931b8
Cleriston-cmd <c.andrade6533@gmail.com>
2024-11-06 11:26:55 -0300

    Modificando Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import { Home } from './pages/home'
+import { Login } from './pages/login'
+import { GlobalStyle } from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      Inicio
-    </div>
+    <Router>
+     <GlobalStyle />
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        
+     </Routes >
+    </Router>
   );
 }
 
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    return (<>
+        <h1>Login</h1>
+        <Link to="/">ir para o home</Link>
+    </>)
+}
+
+export { Login }
